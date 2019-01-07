d00dc16090af77cfb6f54c271e48d8e753ba3e2d
Enteller <evr123@inbox.ru>
2019-01-07 03:44:59 +0300

    Moved to LENINPORN v NONE

---
+++ LENINPORN.py
@@ -0,0 +1,7 @@
+
+
+print("\n m      mmmmmm mm   m mmmmm  mm   m mmmmm   mmmm  mmmmm  mm   m ")
+print(" #      #      #\"m  #   #    #\"m  # #   \"# m\"  \"m #   \"# #\"m  #")
+print(" #      #mmmmm # #m #   #    # #m # #mmm#\" #    # #mmmm\" # #m #")
+print(" #      #      #  # #   #    #  # # #      #    # #   \"m #  # #")
+print(" #mmmmm #mmmmm #   ## mm#mm  #   ## #       #mm#  #    \" #   ## \n\n")
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # hello-world
-Leave it here. This repo is not important  >_&lt;
+OMG This is LENINPORN Pomfff >:33333 >_&lt;
