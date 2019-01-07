cfbec1d8e22a62cdc2ad2a8142885687cb1392be
Enteller <evr123@inbox.ru>
2019-01-07 04:43:31 +0300

    Move to module-like

--- LENINPORN.py
+++ LENINPORN.py
@@ -1,7 +1,11 @@
+#unusable script
+def welcome():
+    print("\n m      mmmmmm mm   m mmmmm  mm   m mmmmm   mmmm  mmmmm  mm   m ")
+    print(" #      #      #\"m  #   #    #\"m  # #   \"# m\"  \"m #   \"# #\"m  #")
+    print(" #      #mmmmm # #m #   #    # #m # #mmm#\" #    # #mmmm\" # #m #")
+    print(" #      #      #  # #   #    #  # # #      #    # #   \"m #  # #")
+    print(" #mmmmm #mmmmm #   ## mm#mm  #   ## #       #mm#  #    \" #   ## \n\n")
 
 
-print("\n m      mmmmmm mm   m mmmmm  mm   m mmmmm   mmmm  mmmmm  mm   m ")
-print(" #      #      #\"m  #   #    #\"m  # #   \"# m\"  \"m #   \"# #\"m  #")
-print(" #      #mmmmm # #m #   #    # #m # #mmm#\" #    # #mmmm\" # #m #")
-print(" #      #      #  # #   #    #  # # #      #    # #   \"m #  # #")
-print(" #mmmmm #mmmmm #   ## mm#mm  #   ## #       #mm#  #    \" #   ## \n\n")
+if __name__ == "__main__":
+    welcome()
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # hello-world
-OMG This is LENINPORN Pomfff >:33333 >_&lt;
+OMG This is LENINPORN Pomfff >:33333
