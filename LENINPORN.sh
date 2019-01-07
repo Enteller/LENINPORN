2943fa689ab2a7184672ca94989871d11686b83a
Enteller <evr123@inbox.ru>
2019-01-07 21:10:56 +0300

    Version 0.0.1

---
+++ LENINPORN.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+toilet LENIN PORN | lolcat -a -s 99;
+
+sleep 1
+while true; do
+
+for ((p=0; p<10;p++))
+do
+
+for ((i=1; i<4; i++))
+do
+clear
+jp2a --size=70x25 pics/$i.jpeg | lolcat;
+sleep 0.3;
+done
+done
+done;
